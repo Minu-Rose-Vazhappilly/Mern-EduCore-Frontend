7814a7e5e984365e0b4d9f3b6b32c24b1a985b95
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-11-19 11:30:47 +0530

    #feat:DEsigned pnf

--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Pnf() {
   return (
-    <div>Pnf</div>
+    <div className='flex justify-center items-center'><img src="https://i.pinimg.com/originals/11/dc/96/11dc96d2e4daca3ea8ff6aa41299b5e1.gif" alt="" /> <button className='ms-2' ><Link className='bg-blue-400 rounded p-3 text-white font-bold' to={'/'}
+    >Back to Home</Link></button></div>
   )
 }
 
