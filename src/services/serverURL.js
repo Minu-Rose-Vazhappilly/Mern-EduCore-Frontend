7ef1fb26f33140c2f39f892edef51393c58253e3
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-11-21 15:47:21 +0530

    #feat:Added Deployed DerverUrl

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,4 @@
-const SERVERURL = "http://localhost:3001"
-
+// const SERVERURL = "http://localhost:3001"
+//https://mern-educore-backend.onrender.com/
+const SERVERURL = "https://mern-educore-backend.onrender.com"
 export default SERVERURL
